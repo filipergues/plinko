58b88c61bc052ed178451d897d868f78282d1c41
Nérys AS&T <filipergues@gmail.com>
2020-05-31 16:38:00 +0100

    funcoes teste

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 var canvas = document.getElementById("canvas"),
   context = canvas.getContext("2d");
+
+const somar = (a, b) => {
+  let operando1 = a;
+  let operando2 = b;
+  let soma = operando1 + operando2;
+  return soma;
+};
+
+const multiplicar = (a, b) => {
+  let operando1 = a;
+  let operando2 = b;
+  let multiplicacao = operando1 * operando2;
+  return multiplicacao;
+};
+
+console.log(somar(7, 4));
+console.log(multiplicar(7, 4));

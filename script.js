1af2ae6facbd6b78168c0d176e385f9b7b3f02ab
Nérys AS&T <filipergues@gmail.com>
2020-05-31 16:59:59 +0100

    Comentarios

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 var canvas = document.getElementById("canvas"),
   context = canvas.getContext("2d");
 
+// Sum function
 const somar = (a, b) => {
   return a + b;
 };
 
+// Mult function
 const multiplicar = (a, b) => {
   return a * b;
 };
 
+// Print in screen
 console.log(somar(7, 4));
 console.log(multiplicar(7, 4));

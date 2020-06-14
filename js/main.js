79c882ea69f136a286956813353bdcaafc327f15
Nérys AS&T <filipergues@gmail.com>
2020-06-14 23:11:33 +0100

    Animação inicial com gravidade

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 var canvas = document.getElementById("canvas"),
   context = canvas.getContext("2d");
 
+// Valores da gravidade e ricochete
+var gravity = 0.2;
+var bounce = -0.6;
+
 // Centro do canvas
 var cx = canvas.width / 2;
 var cy = canvas.height / 2;
@@ -10,77 +14,126 @@ var largura = 450;
 var altura = 550;
 var espessura = 20; // Espessura da borda
 var cor_madeira = "#e6d2b5"; // Bege
-
 // Cria tabuleiro de 450x550 com 20 de espessura da borda
 var tabuleiro = new Tabuleiro(largura, altura, espessura, cor_madeira);
 // Posiciona o tabuleiro no centro do canvas
 tabuleiro.x = cx;
 tabuleiro.y = cy;
-tabuleiro.draw(context);
+// Limites do tabuleiro
+var tabuleiroBounds = tabuleiro.getBounds();
+function drawTabuleiro() {
+  tabuleiro.draw(context);
+}
 
 // Cria 7 gavetas de 50x50
-for (let g = 0; g < 7; g++) {
-  var gaveta_central = new Gaveta(50, 50, "blue");
-  gaveta_central.x = cx - largura / 2 + espessura + 60 * g;
-  gaveta_central.y = cy + altura / 2 - espessura;
-  gaveta_central.draw(context);
+function drawGavetas() {
+  for (let g = 0; g < 7; g++) {
+    var gaveta_central = new Gaveta(50, 50, "blue");
+    gaveta_central.x = cx - largura / 2 + espessura + 60 * g;
+    gaveta_central.y = cy + altura / 2 - espessura;
+    gaveta_central.draw(context);
+  }
 }
 
 // Cria divisórias entre as gavetas
-for (var d = 0; d < 6; d++) {
-  var divisoria = new Divisoria(10, 50, cor_madeira);
-  divisoria.x = cx - largura / 2 + 70 + 60 * d;
-  divisoria.y = cy + altura / 2 - 70;
-  divisoria.draw(context);
+function drawDivisorias() {
+  for (var d = 0; d < 6; d++) {
+    var divisoria = new Divisoria(10, 50, cor_madeira);
+    divisoria.x = cx - largura / 2 + 70 + 60 * d;
+    divisoria.y = cy + altura / 2 - 70;
+    divisoria.draw(context);
+  }
 }
 
 // Cria 4 tabelas de cada lado
-for (var t = 0; t < 4; t++) {
-  // Terceiro argumento com valor 1 refere-se à posição esquerda
-  var tabela_esquerda = new Tabela(30, 120, 1, cor_madeira);
-  tabela_esquerda.x = cx - largura / 2 + espessura;
-  tabela_esquerda.y = cy - altura / 2 + 120 * t;
-  tabela_esquerda.draw(context);
-  // Terceiro argumento com valor -1 refere-se à posição direita
-  var tabela_direita = new Tabela(30, 120, -1, cor_madeira);
-  tabela_direita.x = cx + largura / 2 - espessura;
-  tabela_direita.y = cy - altura / 2 + 120 * t;
-  tabela_direita.draw(context);
+function drawTabelas() {
+  for (var t = 0; t < 4; t++) {
+    // Terceiro argumento com valor 1 refere-se à posição esquerda
+    var tabela_esquerda = new Tabela(30, 120, 1, cor_madeira);
+    tabela_esquerda.x = cx - largura / 2 + espessura;
+    tabela_esquerda.y = cy - altura / 2 + 120 * t;
+    tabela_esquerda.draw(context);
+    // Terceiro argumento com valor -1 refere-se à posição direita
+    var tabela_direita = new Tabela(30, 120, -1, cor_madeira);
+    tabela_direita.x = cx + largura / 2 - espessura;
+    tabela_direita.y = cy - altura / 2 + 120 * t;
+    tabela_direita.draw(context);
+  }
 }
 
 // Cria 7 linhas com 5 ou 6 pinos alternados
-for (var i = 0; i < 7; i++) {
-  for (var j = 0; j < 8; j++) {
-    // Cria pino com raio 5
-    var pino = new Pino(5, cor_madeira);
-    // Se for uma linha par, o primeiro pino
-    // da linha começa na posição x = 105
-    if (i % 2 == 0) pino.x = cx - largura / 2 + 105 + 60 * j;
-    // Se for uma linha impar, o primeiro pino
-    // da linha começa na posição x = 75
-    else pino.x = cx - largura / 2 + 75 + 60 * j;
-    pino.y = cy - altura / 2 + 60 + 60 * i;
-    // Desenha apenas os pinos dentro da area util do tabuleiro
-    if (pino.x < cx + largura / 2 - 50) {
-      pino.draw(context);
+function drawPinos() {
+  for (var i = 0; i < 7; i++) {
+    for (var j = 0; j < 8; j++) {
+      // Cria pino com raio 5
+      var pino = new Pino(5, cor_madeira);
+      // Se for uma linha par, o primeiro pino
+      // da linha começa na posição x = 105
+      if (i % 2 == 0) pino.x = cx - largura / 2 + 105 + 60 * j;
+      // Se for uma linha impar, o primeiro pino
+      // da linha começa na posição x = 75
+      else pino.x = cx - largura / 2 + 75 + 60 * j;
+      pino.y = cy - altura / 2 + 60 + 60 * i;
+      // Desenha apenas os pinos dentro da area util do tabuleiro
+      if (pino.x < cx + largura / 2 - 50) {
+        pino.draw(context);
+      }
     }
   }
 }
 
 // Cria display com a pontuação
-var pontuacao = new Pontuacao(200, 100, "blue");
-pontuacao.x = 50;
-pontuacao.y = 50;
-pontuacao.draw(context);
+function drawPontuacao() {
+  var pontuacao = new Pontuacao(200, 100, "blue");
+  pontuacao.x = 50;
+  pontuacao.y = 50;
+  pontuacao.draw(context);
+}
 
 // Cria Texto com apresentação do jogo
-var texto = new Texto(200, 100, "red");
-texto.x = cx + largura / 2 + 50;
-texto.y = cy - altura / 2;
-texto.draw(context);
+function drawTexto() {
+  var texto = new Texto(200, 100, "red");
+  texto.x = cx + largura / 2 + 50;
+  texto.y = cy - altura / 2;
+  texto.draw(context);
+}
 
 // Cria disco com raio 20 e cor vermelha
+
 var disco = new Disco(20, "red");
 disco.x = cx;
 disco.y = 50;
-disco.draw(context);
+
+// Animação
+(function drawFrame() {
+  window.requestAnimationFrame(drawFrame, canvas);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  //normal motion code
+  disco.vy += gravity;
+  disco.x += disco.vx;
+  disco.y += disco.vy;
+  //bounce off ceiling, floor, and walls
+  if (disco.x + disco.radius > tabuleiroBounds.width) {
+    disco.x = tabuleiroBounds.width - disco.radius;
+    disco.vx *= bounce;
+  } else if (disco.x - disco.radius < tabuleiroBounds.x) {
+    disco.x = disco.radius;
+    disco.vx *= bounce;
+  }
+  if (disco.y + disco.radius > tabuleiroBounds.height) {
+    disco.y = tabuleiroBounds.height - disco.radius;
+    disco.vy *= bounce;
+  } else if (disco.y - disco.radius < 0) {
+    disco.y = disco.radius;
+    disco.vy *= bounce;
+  }
+  drawTabuleiro();
+  drawGavetas();
+  drawDivisorias();
+  drawTabelas();
+  drawPinos();
+  drawPontuacao();
+  drawTexto();
+  disco.draw(context);
+})();

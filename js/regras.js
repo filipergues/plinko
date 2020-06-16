27f12a5c66d563020ad33562eba0d2f15bf6a1f5
Wenderson Wanzeller <wenderson@wanzeller.com.br>
2020-06-16 20:52:20 +0100

    RETIRADO TEXTO PONTUACAO

--- js/regras.js
+++ js/regras.js
@@ -19,48 +19,47 @@ Regras.prototype.draw = function (context) {
   context.font = "24px Arial";
   context.textAlign = "start";
   context.textBaseline = "middle";
-  context.fillText("PONTUAÇÃO", 0, this.altura / 2 - 10);
+  context.fillText("PONTUAÇÃO", 0, this.altura / 2 );
   context.closePath();
 
   context.beginPath();
   context.font = "12px Arial";
   context.textAlign = "start";
   context.textBaseline = "middle";
-  context.fillText("Nossas gavetas valem Euros:", 0, this.altura / 2 + 25);
   context.fillText(
     `1ª - ${this.pontos[0]} Pontos = ${this.pontos[0]} €`,
     0,
-    this.altura / 2 + 60
+    this.altura / 2 + 40
   );
   context.fillText(
     `2ª - ${this.pontos[1]} Pontos = ${this.pontos[1]} €`,
     0,
-    this.altura / 2 + 80
+    this.altura / 2 + 60
   );
   context.fillText(
     `3ª - ${this.pontos[2]} Pontos = ${this.pontos[2]} €`,
     0,
-    this.altura / 2 + 100
+    this.altura / 2 + 80
   );
   context.fillText(
     `4ª - ${this.pontos[3]} Pontos = ${this.pontos[3]} €`,
     0,
-    this.altura / 2 + 120
+    this.altura / 2 + 100
   );
   context.fillText(
     `5ª - ${this.pontos[4]} Pontos = ${this.pontos[4]} €`,
     0,
-    this.altura / 2 + 140
+    this.altura / 2 + 120
   );
   context.fillText(
     `6ª - ${this.pontos[5]} Pontos = ${this.pontos[5]} €`,
     0,
-    this.altura / 2 + 160
+    this.altura / 2 + 140
   );
   context.fillText(
     `7ª - ${this.pontos[6]} Pontos = ${this.pontos[6]} €`,
     0,
-    this.altura / 2 + 180
+    this.altura / 2 + 160
   );
   context.closePath();
 

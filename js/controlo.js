b902fa5849fbab9c97bb4fdc2693d71e390a5fb0
Nérys AS&T <filipergues@gmail.com>
2020-06-16 19:13:56 +0100

    Iniciar e Repor

--- js/controlo.js
+++ js/controlo.js
@@ -1,5 +1,14 @@
 var botaoInicio = document.getElementById("iniciar");
+var botaoRepor = document.getElementById("repor");
 
 botaoInicio.onclick = function (e) {
-  console.log("Iniciar");
+  gravity = 0.2;
+};
+
+botaoRepor.onclick = function (e) {
+  gravity = 0;
+  disco.x = cx;
+  disco.y = 45;
+  disco.vx = 0;
+  disco.vy = 0;
 };

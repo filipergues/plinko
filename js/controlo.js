c44e7c295646a97611c53b6dd0fcb7e229a841e8
Nérys AS&T <filipergues@gmail.com>
2020-06-17 18:50:42 +0100

    Comentários

--- js/controlo.js
+++ js/controlo.js
@@ -1,10 +1,12 @@
 var botaoInicio = document.getElementById("iniciar");
 var botaoRepor = document.getElementById("repor");
 
+// Clicar no botão para iniciar o jogo
 botaoInicio.onclick = function (e) {
   gravity = 0.2;
 };
 
+// Clicar no botão para repor o disco à sua posição original
 botaoRepor.onclick = function (e) {
   gravity = 0;
   disco.x = cx;
